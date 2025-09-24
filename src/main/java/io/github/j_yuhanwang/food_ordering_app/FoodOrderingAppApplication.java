29a516911e0999e426ff559f97b93e68c9e4f5b3
YuhanW <wangyuhan6459@163.com>
2025-09-24 16:56:15 +0100

    Initial project setup

---
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/FoodOrderingAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.j_yuhanwang.food_ordering_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/j_yuhanwang/food_ordering_app/FoodOrderingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.j_yuhanwang.food_ordering_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
