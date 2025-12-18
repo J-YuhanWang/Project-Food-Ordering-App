c01b0534e72c1565b50a506f97876937076f5b49
YuhanW <wangyuhan6459@163.com>
2025-12-18 20:52:24 +0000

    feat: add DTOs and entities for food ordering system and fix JPA repository issues

--- src/main/java/io/github/j_yuhanwang/food_ordering_app/response/Response.java
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/response/Response.java
@@ -22,5 +22,5 @@ public class Response<T> {
     private int statusCode; // e.g.,"200", "404"
     private String message; // Additional information about the response
     private T data; // The actual data payload
-    private Map<String, Object> meta; //TODO
+    private Map<String, Object> meta;
 }
