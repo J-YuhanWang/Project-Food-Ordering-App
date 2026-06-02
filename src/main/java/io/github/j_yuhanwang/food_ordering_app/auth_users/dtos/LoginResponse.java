aef768b21ee414706ffd71ef0038c816f8036870
J-YuhanWang <wangyuhan6459@gmail.com>
2026-06-02 22:55:41 +0100

    refactor(auth): update LoginResponse(accessToken, refreshToken) and add RefreshTokenRequest DTO

--- src/main/java/io/github/j_yuhanwang/food_ordering_app/auth_users/dtos/LoginResponse.java
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/auth_users/dtos/LoginResponse.java
@@ -21,6 +21,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class LoginResponse {
-    private String token;
+    private String accessToken;
+    private String refreshToken;
     private List<RoleType> roles;
 }
