d26d67f2e379f5625d9debb3bb2352f5c7be77d2
J-YuhanWang <wangyuhan6459@gmail.com>
2026-06-06 12:28:09 +0100

    feat(notification): implement sendOrderConfirmation()
    Renders order-confirmation Thymeleaf template via
    SpringTemplateEngine, sends via JavaMailSender @Async,
    saves audit log to database.

--- src/main/java/io/github/j_yuhanwang/food_ordering_app/email_notification/services/NotificationService.java
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/email_notification/services/NotificationService.java
@@ -1,6 +1,7 @@
 package io.github.j_yuhanwang.food_ordering_app.email_notification.services;
 
 import io.github.j_yuhanwang.food_ordering_app.email_notification.dtos.NotificationDTO;
+import io.github.j_yuhanwang.food_ordering_app.order.dtos.OrderDTO;
 
 /**
  * @author YuhanWang
@@ -8,4 +9,5 @@ import io.github.j_yuhanwang.food_ordering_app.email_notification.dtos.Notificat
  */
 public interface NotificationService {
     void sendVerificationEmail(NotificationDTO notificationDTO);
+    void sendOrderConfirmation(OrderDTO orderDTO, String userEmail);
 }
