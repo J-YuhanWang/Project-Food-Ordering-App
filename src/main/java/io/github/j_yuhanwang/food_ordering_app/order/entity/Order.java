1d4367aa2e09b9877b4aaded3124ed419154eefd
J-YuhanWang <wangyuhan6459@gmail.com>
2026-06-07 15:52:51 +0100

    perf(order): resolve memory pagination and N+1 query issue
    - Removed @EntityGraph from paginated query methods in OrderRepository to prevent Hibernate from fetching collections in memory.
    - Applied @BatchSize(size = 50) on orderItems collection in Order entity to optimize lazy loading via batched IN clauses.

--- src/main/java/io/github/j_yuhanwang/food_ordering_app/order/entity/Order.java
+++ src/main/java/io/github/j_yuhanwang/food_ordering_app/order/entity/Order.java
@@ -7,6 +7,7 @@ import io.github.j_yuhanwang.food_ordering_app.enums.PaymentStatus;
 import io.github.j_yuhanwang.food_ordering_app.payment.entity.Payment;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -96,6 +97,7 @@ public class Order {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     @ToString.Exclude
+    @BatchSize(size = 50)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     /**
