59098b1dbdd1b24058fef50acf65dc30513b25d9
J-YuhanWang <wangyuhan6459@gmail.com>
2026-08-22 21:56:23 +0100

    fix(next-config): whitelist S3 domain for canteen cover images

--- frontend/customer-app/next.config.mjs
+++ frontend/customer-app/next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
     remotePatterns:[
       {protocol: 'https', hostname: 'images.unsplash.com'},
       {protocol: 'https', hostname: 'images.pexels.com'},
+      {protocol: 'https', hostname: 'ucd-canteen-app-dev.s3.eu-west-1.amazonaws.com' },
     ],
   },
 }
